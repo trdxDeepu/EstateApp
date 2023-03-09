6e46fdc3a6f0e521d02bce1cccac1f9dbe5f3a94
Trdx Lucifer <singhdepu566@gmail.com>
2023-03-10 00:48:25 +0530

    make header added router and uselocation to get path and make header alos

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import { BrowserRouter , Route , Routes } from 'react-router-dom'
-import Home from './Pages/Home'
-import Offer from './Pages/Offer'
-import Profile from './Pages/Profile'
-import SignIn from './SigninSignup/SignIn'
-import SignUp from './SigninSignup/SignUp'
-import ForgotPassword from './SigninSignup/ForgotPassword'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Offer from "./Pages/Offer";
+import Profile from "./Pages/Profile";
+import SignIn from "./SigninSignup/SignIn";
+import SignUp from "./SigninSignup/SignUp";
+import ForgotPassword from "./SigninSignup/ForgotPassword";
+import Header from "./Components/Header"
 
-
-const Router = () => {
+const AppRouter = () => {
   return (
-    <BrowserRouter>
-    <Routes>
-        <Route path ="/" element={<Home/>}/>
-        <Route path ="/offer" element={<Offer/>}/>
-        <Route path ="/profile" element={<Profile/>}/>
-        <Route path ="/signin" element={<SignIn/>}/>
-        <Route path ="/signup" element={<SignUp/>}/>
-        <Route path ="/forgot" element={<ForgotPassword/>}/>
-    </Routes>
-    </BrowserRouter>
-  )
-}
+    <>
+     <Router>
+     <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/offer" element={<Offer />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      </Routes>
+    </Router>
+    </>
+   
+  );
+};
 
-export default Router
+export default AppRouter;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-       <h1 className="text-red-600">Real estate app</h1>
- <h1 className="text-3xl text-red-600 bg-yellow-300">ho world </h1>
-
+     
     </div>
   )
 }
