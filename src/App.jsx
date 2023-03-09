7990d05d1b9e38ab7f06a4440a38402868730d81
Trdx Lucifer <singhdepu566@gmail.com>
2023-03-09 22:35:46 +0530

    added h2 tags

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
  <>
  <h1 className="text-red-600">Real estate app</h1>
- <h1 className="text-3xl text-red-600 bg-yellow-300">hello world </h1>
+ <h1 className="text-3xl text-red-600 bg-yellow-300">ho world </h1>
 
  </>
   )
