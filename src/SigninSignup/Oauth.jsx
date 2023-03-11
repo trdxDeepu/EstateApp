9456705d8f161b6a26929fdf5fb912e9f8f5b322
Trdx Lucifer <singhdepu566@gmail.com>
2023-03-11 16:10:46 +0530

    added google authentication

--- src/SigninSignup/Oauth.jsx
+++ src/SigninSignup/Oauth.jsx
@@ -1,9 +1,41 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { db } from "../Firebase";
 
 const Oauth = () => {
+  const navigate = useNavigate();
+  const OnGoogleClick = async () => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+
+      toast.success("Signed in successfully");
+      navigate("/");
+    } catch (err) {
+      toast.error("error signing in");
+    }
+  };
+
   return (
     <button
+      type="button"
+      onClick={OnGoogleClick}
       className="flex items-center justify-center
     w-full bg-red-700 text-white px-7 py-3  uppercase
      text-sm font-serif font-medium
