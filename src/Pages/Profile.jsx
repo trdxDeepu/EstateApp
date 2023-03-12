d8b4ace09c0ddb5e772c48d47670281f02b3b2fb
Trdx Lucifer <singhdepu566@gmail.com>
2023-03-12 19:40:04 +0530

    added profile and added some tags

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Profile = () => {
+
+  const [formData , setFormData] = useState({
+    name:"Depu",
+    email:"singhdepu566@gmail.com"
+  })
+
   return (
-    <div>Profile</div>
+   <section>
+    <h1 className='text-3xl text-center mt-6 font-serif font-bold'>My Profile</h1>
+    <div>
+      <form >
+      <input type="text" 
+      id="name"
+      value={name}
+      />
+      </form>
+    </div>
+   </section>
   )
 }
 
