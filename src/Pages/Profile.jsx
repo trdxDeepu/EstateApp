2b7c92871026b2799189689cfbfe854d06f2badf
Trdx Lucifer <singhdepu566@gmail.com>
2023-03-12 19:50:38 +0530

    makes the input center and make the added the tailwind css

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -10,20 +10,32 @@ const Profile = () => {
 
   return (
     <>
-      <section className="mx-w-6xl mx-auto-flex justify-center items-center flex-col">
+      <section className="mx-w-6xl mx-auto flex justify-center items-center flex-col">
         <h1 className="text-3xl text-center mt-6 font-serif font-bold">
           My Profile
         </h1>
         <div className="w-full md:w-[50%] mt-6 px-3">
           <form>
+            {/* name input*/}
             <input
               type="text"
               id="name"
               value={name}
               disabled
               className="w-full px-4 py-2 text-xl text-gray-700
+      bg-white border border-gray-300 rounder-sm
+       transition ease-in-out mb-6"
+            />
+            {/* email input */}
+            <input
+              type="email"
+              id="email"
+              value={email}
+              disabled
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700
       bg-white border border-gray-300 rounder-sm transition ease-in-out"
             />
+
           </form>
         </div>
       </section>
