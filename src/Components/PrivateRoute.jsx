ab7208af57e95be5c257efab184543ba5ecc4c93
Trdx Lucifer <singhdepu566@gmail.com>
2023-03-12 20:23:42 +0530

    created own hook for authentication to chekc in  using auth and change the loading status

--- src/Components/PrivateRoute.jsx
+++ src/Components/PrivateRoute.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { Navigate, Outlet } from 'react-router';
+import { Outlet, Navigate } from "react-router-dom";
+import { useAuthStatus } from "../hooks/useAuthStatus";
 
-const PrivateRoute = () => {
-    const loggedIn = false;
-
-  return loggedIn ? <Outlet/> : <Navigate to="/sign-in"/>
+export default function PrivateRoute() {
+  const { loggedIn, checkingStatus } = useAuthStatus();
+  if (checkingStatus) {
+    return <h3>Loading......</h3>;
+  }
+  return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
 }
-
-export default PrivateRoute
--- src/Hooks/UseAuthStatus.jsx
+++ src/Hooks/UseAuthStatus.jsx
@@ -1,12 +1,13 @@
+import { useEffect, useState } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import React, { useEffect, useState } from "react";
 
-const UseAuthState = () => {
+export function useAuthStatus() {
   const [loggedIn, setLoggedIn] = useState(false);
   const [checkingStatus, setCheckingStatus] = useState(true);
 
   useEffect(() => {
     const auth = getAuth();
+    console.log(auth);
     onAuthStateChanged(auth, (user) => {
       if (user) {
         setLoggedIn(true);
@@ -14,8 +15,5 @@ const UseAuthState = () => {
       setCheckingStatus(false);
     });
   }, []);
-
-  return { loggedIn, setCheckingStatus };
-};
-
-export default UseAuthState;
+  return { loggedIn, checkingStatus };
+}
