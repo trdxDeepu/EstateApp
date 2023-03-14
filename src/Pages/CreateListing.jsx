79a0375b5e46d88216156ea4c648dda0387c8a76
Trdx Lucifer <singhdepu566@gmail.com>
2023-03-14 15:02:38 +0530

    added regular price input

--- src/Pages/CreateListing.jsx
+++ src/Pages/CreateListing.jsx
@@ -8,9 +8,24 @@ const CreateListing = () => {
     bathrooms: 1,
     parking: false,
     furnished: true,
+    address: "",
+    description: "",
+    offer: false,
+    regularPrice: 0,
   });
 
-  const { type, name, bedrooms, bathrooms, parking, furnished } = formData;
+  const {
+    type,
+    name,
+    bedrooms,
+    bathrooms,
+    parking,
+    furnished,
+    address,
+    description,
+    offer,
+    regularPrice,
+  } = formData;
 
   const OnChange = () => {};
 
@@ -104,23 +119,19 @@ const CreateListing = () => {
             className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md
          rounded hover:shadow-lg focus:shadow-lg active:shadow-lg 
          transition duration-150 ease-in-out w-full 
-         ${
-           !parking ? "bg-white text-black" : "bg-slate-600 text-white"
-         }`}
+         ${!parking ? "bg-white text-black" : "bg-slate-600 text-white"}`}
             id="parking"
             value={true}
             onClick={OnChange}
           >
-           Yes
+            Yes
           </button>
           <button
             type="button"
             className={` ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md
          rounded hover:shadow-lg focus:shadow-lg active:shadow-lg 
          transition duration-150 ease-in-out w-full 
-         ${
-            parking ? "bg-white text-black" : "bg-slate-600 text-white"
-         }`}
+         ${parking ? "bg-white text-black" : "bg-slate-600 text-white"}`}
             id="parking"
             value={false}
             onClick={OnChange}
@@ -135,29 +146,104 @@ const CreateListing = () => {
             className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md
          rounded hover:shadow-lg focus:shadow-lg active:shadow-lg 
          transition duration-150 ease-in-out w-full 
-         ${
-           !furnished ? "bg-white text-black" : "bg-slate-600 text-white"
-         }`}
+         ${!furnished ? "bg-white text-black" : "bg-slate-600 text-white"}`}
             id="furnished"
             value={true}
             onClick={OnChange}
           >
-           Yes
+            Yes
           </button>
           <button
             type="button"
             className={` ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md
          rounded hover:shadow-lg focus:shadow-lg active:shadow-lg 
          transition duration-150 ease-in-out w-full 
-         ${
-           furnished ? "bg-white text-black" : "bg-slate-600 text-white"
-         }`}
+         ${furnished ? "bg-white text-black" : "bg-slate-600 text-white"}`}
             id="furnished"
             value={false}
             onClick={OnChange}
           >
             No
           </button>
+        </div>
+        <p className="text-lg mt-6 font-semibold font-sans">Address</p>
+        <textarea
+          type="text"
+          id="address"
+          value={address}
+          onChange={OnChange}
+          placeholder="Address"
+          required
+          className=" mt-3 w-full px-4 py-2 text-xl text-gray-700
+        bg-white border-gray-300 transition duration-150 ease-in-out rounded
+        focus:text-gray-700 focus:bg-white focus:border-slate-300 mb-6 "
+        />
+        <p className="text-lg  font-semibold font-sans">Description</p>
+        <textarea
+          type="text"
+          id="description"
+          value={description}
+          onChange={OnChange}
+          placeholder=" Description"
+          required
+          className=" mt-3 w-full px-4 py-2 text-xl text-gray-700
+        bg-white border-gray-300 transition duration-150 ease-in-out rounded
+        focus:text-gray-700 focus:bg-white focus:border-slate-300 mb-6 "
+        />
+        <p className="text-lg font-sans font-semibold">Offers</p>
+        <div className="flex mt-3 ">
+          <button
+            type="button"
+            className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md
+         rounded hover:shadow-lg focus:shadow-lg active:shadow-lg 
+         transition duration-150 ease-in-out w-full 
+         ${!offer ? "bg-white text-black" : "bg-slate-600 text-white"}`}
+            id="offer"
+            value={true}
+            onClick={OnChange}
+          >
+            Yes
+          </button>
+          <button
+            type="button"
+            className={` ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md
+         rounded hover:shadow-lg focus:shadow-lg active:shadow-lg 
+         transition duration-150 ease-in-out w-full 
+         ${offer ? "bg-white text-black" : "bg-slate-600 text-white"}`}
+            id="furnished"
+            value={false}
+            onClick={OnChange}
+          >
+            No
+          </button>
+        </div>
+        <div className="mt-6 flex items-center mb-6 ">
+          <div className=" ">
+            <p className="text-lg font-sans font-semibold ">Regular Price</p>
+            <div className=" flex w-full justify-center items-center space-x-6">
+              <input
+                type="number"
+                id="regularPrice"
+                value={regularPrice}
+                onChange={OnChange}
+                min="50"
+                max="40000000"
+                required
+                className="w-full px-4 py-2 text-xl text-gray-700 bg-white
+              border border-gray-300 rounded transition duration-150 ease-in-out
+              focus:bg-white focus:border-slate-600 focus:text-gray-700 text-center
+              "
+              />
+              {type === "rent" && (
+                <div>
+                  <p className="text-md w-full whitespace-nowrap ">
+                    {" "}
+                    $ / Month
+                  </p>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
       </form>
     </main>
