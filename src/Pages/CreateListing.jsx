d54d7590b306204274f93db832be768a0719fe49
Trdx Lucifer <singhdepu566@gmail.com>
2023-03-14 12:25:16 +0530

    created two button on listing page for sell and rent and make them dynamic type

--- src/Pages/CreateListing.jsx
+++ src/Pages/CreateListing.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const CreateListing = () => {
-  return (
-    <div>CreateListing</div>
-  )
-}
+  const [formData, setFormData] = useState({
+    type: "rent",
+  });
 
-export default CreateListing
+  const { type } = formData;
+
+  const OnChange = () => {};
+
+  return (
+    <main className="max-w-md px-2 mx-auto ">
+      <h1 className="text-3xl font-serif text-center mt-6 font-bold">
+        Create Listing
+      </h1>
+      <form>
+        <p className="text-lg mt-6 font-sans font-semibold">Sell / Rent</p>
+        <div className="flex mt-3 ">
+          <button
+            type="button"
+            className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md
+         rounded hover:shadow-lg focus:shadow-lg active:shadow-lg 
+         transition duration-150 ease-in-out w-full 
+         ${
+           type === "sale" ? "bg-white text-black" : "bg-slate-600 text-white"
+         }`}
+            id="type"
+            value="sale"
+            onClick={OnChange}
+          >
+            Sell
+          </button>
+          <button
+            type="button"
+            className={` ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md
+         rounded hover:shadow-lg focus:shadow-lg active:shadow-lg 
+         transition duration-150 ease-in-out w-full 
+         ${
+           type === "rent" ? "bg-white text-black" : "bg-slate-600 text-white"
+         }`}
+            id="type"
+            value="sale"
+            onClick={OnChange}
+          >
+            Rent
+          </button>
+        </div>
+      </form>
+    </main>
+  );
+};
+
+export default CreateListing;
