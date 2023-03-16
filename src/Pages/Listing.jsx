68a316c89ef5182e2238d86328c2466ec29c81af
Trdx Lucifer <singhdepu566@gmail.com>
2023-03-16 13:49:23 +0530

    added swipper bundles and css to style the page and the pics to slide in the page of listing

--- src/Pages/Listing.jsx
+++ src/Pages/Listing.jsx
@@ -1,39 +1,64 @@
-import { doc, getDoc } from 'firebase/firestore'
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router'
-import Spinner from '../Components/Spinner'
-import { db } from '../Firebase'
+import { doc, getDoc } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import Spinner from "../Components/Spinner";
+import { db } from "../Firebase";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, {
+  EffectFade,
+  Autoplay,
+  Navigation,
+  Pagination,
+} from "swiper";
+import "swiper/css/bundle";
 
 const Listing = () => {
-    const [listing,setListing] = useState(null)
-    const [loading,setLoading] = useState(true)
-    const params = useParams()
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const params = useParams();
 
-    useEffect(()=>{
+  SwiperCore.use([Autoplay, Navigation, Pagination]);
 
-        async function fetchListing(){
-            const docRef = doc(db,"listings",params.listingId)
-            const docSnap = await getDoc(docRef)
-            if(docSnap.exists()){
-                setListing(docSnap.data())
-                setLoading(false)
-                
-
-            }
-
-        }
-        fetchListing();
-        
-
-    },[params.listingID])
-
-    if(loading){
-        return <Spinner/>
+  useEffect(() => {
+    async function fetchListing() {
+      const docRef = doc(db, "listings", params.listingId);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setListing(docSnap.data());
+        setLoading(false);
+      }
     }
+    fetchListing();
+  }, [params.listingID]);
+
+  if (loading) {
+    return <Spinner />;
+  }
 
   return (
-    <div>{listing.name}</div>
-  )
-}
+    <main>
+      <Swiper
+        slidesPerView={1}
+        navigation
+        pagination={{ type: "progressbar" }}
+        effect="fade"
+        modules={[EffectFade]}
+        autoplay={{ delay: 3000 }}
+      >
+        {listing.imgUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              className="relative w-full overflow-hidden h-[300px]"
+              style={{
+                background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                backgroundSize: "cover",
+              }}
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </main>
+  );
+};
 
-export default Listing
+export default Listing;
