3c8e893fff96ebd2030a59405863f479c18514f9
Trdx Lucifer <singhdepu566@gmail.com>
2023-03-16 17:57:13 +0530

    added a contactLandlord button so any user who is not signed in can contact the landlord

--- src/Pages/Listing.jsx
+++ src/Pages/Listing.jsx
@@ -20,8 +20,11 @@ import {
   FaParking,
   FaChair,
 } from "react-icons/fa";
+import { getAuth } from "firebase/auth";
 
 const Listing = () => {
+  const auth = getAuth();
+  const [contactLandlord,setContactLandlord] = useState(false)
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
   const [shareLinkCopy, setShareLinkCopy] = useState(false);
@@ -139,6 +142,19 @@ const Listing = () => {
               {listing.furnished ? "Furnished" : "Not furnished"}
             </li>
           </ul>
+          {listing.userRef !== auth.currentUser?.uid && !contactLandlord && (
+            <div className="mt-6">
+              <button
+                onClick={() => setContactLandlord(true)}
+                className="px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg w-full text-center transition duration-150 ease-in-out "
+              >
+                Contact Landlord
+              </button>
+            </div>
+          )}
+          {contactLandlord && (
+            <Contact userRef={listing.userRef} listing={listing} />
+          )}
         </div>
         <div className="bg-blue-300 h-[200px] w-full lg-[400px]"></div>
       </div>
