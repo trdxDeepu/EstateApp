a95d7f7dcc63fb75e898d645b56ec43ad7562436
Trdx Lucifer <singhdepu566@gmail.com>
2023-03-17 12:03:18 +0530

    added offer page and make offer to display the item first used useEffect to take offer data in array so we map and display using listing''item

--- src/Components/ListingItem.jsx
+++ src/Components/ListingItem.jsx
@@ -31,7 +31,7 @@ export default function ListingItem({ listing, id, onEdit, onDelete }) {
           <p className="font-semibold m-0 text-xl truncate">{listing.name}</p>
           <p className="text-[#457b9d] mt-2 font-semibold">
             $
-            {listing.offer
+            {listing.offerm
               ? listing.discountedPrice
                   .toString()
                   .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
