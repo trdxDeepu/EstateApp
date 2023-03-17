9c8e8349d48fb85b33313325b7ef40614c1f41d0
Trdx Lucifer <singhdepu566@gmail.com>
2023-03-17 12:27:55 +0530

    some changes

--- src/Components/PrivateRoute.jsx
+++ src/Components/PrivateRoute.jsx
@@ -1,5 +1,5 @@
 import { Outlet, Navigate } from "react-router-dom";
-import {useAuthStatus} from "../Hooks/useAuthStatus";
+import {useAuthStatus} from "../Hooks/UseAuthStatus";
 import Spinner from "./Spinner";
 
 export default function PrivateRoute() {
